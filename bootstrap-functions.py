33911ac2c8d8c5822ef4486707a94524c39f18be
dominik <dominikpeter@users.noreply.github.com>
2018-03-27 22:42:08 +0200

    Update bootstrap-functions.py

--- bootstrap-functions.py
+++ bootstrap-functions.py
@@ -1,3 +1,19 @@
+def bootstrap_replicate_1d(data, func):
+    sample = np.random.choice(data, size=len(data))
+    return func(sample)
+
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data, func)
+
+    return bs_replicates
+
 def permutation_sample(data1, data2):
     """Generate a permutation sample from two data sets."""
 
