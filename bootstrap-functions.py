c133d544987540e98cb7a47f6a579e080f415b3a
dominik <dominikpeter@users.noreply.github.com>
2018-03-27 22:38:19 +0200

    Update and rename draw_perm_reps.py to bootstrap-functions.py

--- bootstrap-functions.py
+++ bootstrap-functions.py
@@ -28,3 +28,22 @@ def draw_perm_reps(data_1, data_2, func, size=1):
         perm_replicates[i] = func(perm_sample_1, perm_sample_2)
 
     return perm_replicates
+
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x, bs_y, 1)
+
+    return bs_slope_reps, bs_intercept_reps
